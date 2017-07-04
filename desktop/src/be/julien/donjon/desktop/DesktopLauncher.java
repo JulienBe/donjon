a5e86156d9d3f01491ebe6de4d824ae39533d8da
JulienBe <bertozzijulien@gmail.com>
2017-07-04 15:22:02 +0200

    Initial commit

---
+++ desktop/src/be/julien/donjon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package be.julien.donjon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import be.julien.donjon.DonjonMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DonjonMain(), config);
+	}
+}
---
+++ html/src/be/julien/donjon/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package be.julien.donjon.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import be.julien.donjon.DonjonMain;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new DonjonMain();
+        }
+}
